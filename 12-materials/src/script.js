b8989738bb76bab39fc19ae8e03054bfb8e2d711
CSY <dvz1289@naver.com>
2021-10-19 16:27:42 +0900

    textures & Lights

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -1,12 +1,20 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+
+const gui = new dat.GUI()
 
 /**
  * Textures
  */
 
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -20,6 +28,15 @@ const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
 // gradientTexture.minFilter = THREE.NearestFilter
 // gradientTexture.magFilter = THREE.NearestFilter
 // gradientTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/1/px.jpg',
+    '/textures/environmentMaps/1/nx.jpg',
+    '/textures/environmentMaps/1/py.jpg',
+    '/textures/environmentMaps/1/ny.jpg',
+    '/textures/environmentMaps/1/pz.jpg',
+    '/textures/environmentMaps/1/nz.jpg'
+])
 
 /**
  * Base
@@ -50,9 +67,9 @@ const scene = new THREE.Scene()
 // material.gradientMap = gradientTexture
 
 const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.45
-material.roughness = 0.65
 
+material.metalness = 0.7
+material.roughness = 0.2
 // material.map = doorColorTexture
 // material.color.set('#ff00ff')
 // material.color = new THREE.Color('#00ff00')
@@ -61,26 +78,47 @@ material.roughness = 0.65
 // material.opacity = 0.5
 // material.transparent = true
 // material.alphaMap = doorAlphaTexture
-material.side = THREE.DoubleSide
+// material.side = THREE.DoubleSide
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.1, 0.1)
+
+material.envMap = environmentMapTexture
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 64, 64),
     material
 )
 
 sphere.position.x = -1.5
 
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 100, 100),
     material
 )
 
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
     material
 )
 
 torus.position.x = 1.5
+
+torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
 
 scene.add(sphere, plane, torus)
 
@@ -154,13 +192,13 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = 0.3 * elapsedTime
-    plane.rotation.y = 0.3 * elapsedTime
-    torus.rotation.y = 0.3 * elapsedTime
+    sphere.rotation.y = 0.08 * elapsedTime
+    plane.rotation.y = 0.08 * elapsedTime
+    torus.rotation.y = 0.08 * elapsedTime
 
-    sphere.rotation.x = 0.3 * elapsedTime
-    plane.rotation.x = 0.3 * elapsedTime
-    torus.rotation.x = 0.3 * elapsedTime
+    sphere.rotation.x = 0.08 * elapsedTime
+    plane.rotation.x = 0.08 * elapsedTime
+    torus.rotation.x = 0.08 * elapsedTime
 
     // Update controls
     controls.update()
